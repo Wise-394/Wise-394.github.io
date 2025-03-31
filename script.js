14d6561e63a2cb5a374aec5e64bd4d54753e85cf
Wise-394 <114738222+Wise-394@users.noreply.github.com>
2025-03-31 19:58:03 +0800

    working form

---
+++ script.js
@@ -0,0 +1,11 @@
+/* ================= HEADER BEHAVIOUR ================= */
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("header").style.top = "0";
+    } else {
+        document.getElementById("header").style.top = "-90px";
+    }
+    prevScrollpos = currentScrollPos;
+}
